062adea2dea3d00f7169029cd4065e6d6bef1a81
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2020-08-04 21:10:01 +0300

    Customized Admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, TabularInline
+
 from .models import Advert, SellerFeedback, Category, UserAddress, AdvertPhoto, User
 
-admin.site.register(Advert)
-admin.site.register(AdvertPhoto)
-admin.site.register(User)
+
+class UserAddressAdmin(ModelAdmin):
+    list_display = ['owner', 'city', 'street', 'building_number']
+
+
+class AddressInline(TabularInline):
+    model = UserAddress
+    extra = 1
+
+
+class AdvertPhotoInline(TabularInline):
+    model = AdvertPhoto
+    extra = 1
+
+
+class AdvertAdmin(ModelAdmin):
+    fieldsets = [
+        ("Загальне", {"fields": ['title', 'owner', 'price', 'category', 'description', 'address']}),
+        ("Адміністрування", {"fields": ['is_price_final', 'is_draft', 'is_reviewed', 'is_canceled']}),
+    ]
+    inlines = [AdvertPhotoInline]
+
+
+class UserAdmin(ModelAdmin):
+    inlines = [AddressInline]
+
+
+admin.site.register(Advert, AdvertAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserAddress, UserAddressAdmin)
 admin.site.register(SellerFeedback)
 admin.site.register(Category)
-admin.site.register(UserAddress)
 
